37e49d5cde6976434e0d5eb1d922d876a366f359
DomeniGeco <domenigeco@gmail.com>
2026-09-18 19:48:48 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,33 @@
+"""Avvia LetzyOS"""
+
+import os
+
+def on():
+    """Esce dal programma."""
+    os.system("clear")
+    print("Montando filesystem :/tmp/*")
+    print("Aprendo LTemp...")
+    print("Aprendo LetzyUserInterface...")
+    time.sleep(1)
+    print("Aprendo SigtermServiceLand...")
+    print("Smontando filesystem: :/network/*...")
+    print("Aprendo SigtermServiceMSG...")
+    time.sleep(3)
+    print("Montando filesystem: /letzy/*...")
+    time.sleep(0.7)
+    print("Aprendo CmdRunCommands...")
+    print("Aprendo /letzy/core/cmd...")
+    print("Aprendo CmdTermRecognize...")
+    time.sleep(2)
+    print("Connessione come 'user'...")
+    time.sleep(0.5)
+    
+    os.system("clear")
+    
+    
+    
+    
+
+    
+    
+    
